abe7e9406535ac50a39c4b7ba5a0d0790a984307
liubaishui <szliuquan@gmail.com>
2019-08-31 10:44:31 +0800

    Delete X5WebviewPlugin.m

--- ios/Classes/X5WebviewPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "X5WebViewPlugin.h"
-#import <x5_webview/x5_webview-Swift.h>
-
-@implementation X5WebViewPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftX5WebviewPlugin registerWithRegistrar:registrar];
-}
-@end
