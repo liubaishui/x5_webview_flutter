62402f96b859fda6cbf9f99ef2ae6c8b146a7a8a
刘泉 <liuq@29m.net>
2019-10-28 17:06:04 +0800

    fix

--- ios/Classes/X5WebviewPlugin.m
+++ ios/Classes/X5WebviewPlugin.m
@@ -1,7 +1,7 @@
 #import "X5WebviewPlugin.h"
 #import <x5_webview/x5_webview-Swift.h>
 
-@implementation X5WebviewPlugin
+@implementation X5WebViewPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
   [SwiftX5WebviewPlugin registerWithRegistrar:registrar];
 }
