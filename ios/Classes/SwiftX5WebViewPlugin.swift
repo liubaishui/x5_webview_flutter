1ec338465f6eed0003cbaa925c99fd9be2127b35
liubaishui <szliuquan@gmail.com>
2019-12-02 16:44:10 +0800

    Update SwiftX5WebViewPlugin.swift

--- ios/Classes/SwiftX5WebViewPlugin.swift
+++ ios/Classes/SwiftX5WebViewPlugin.swift
@@ -4,7 +4,7 @@ import UIKit
 public class SwiftX5WebViewPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "x5_webview", binaryMessenger: registrar.messenger())
-    let instance = SwiftX5WebviewPlugin()
+    let instance = SwiftX5WebViewPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
