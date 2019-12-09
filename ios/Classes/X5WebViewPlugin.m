2dc815e7e06bf4a7bda0e11742a4e6952c66f1b8
liubaishui <szliuquan@gmail.com>
2019-12-09 20:41:08 +0800

    Update X5WebViewPlugin.m

--- ios/Classes/X5WebViewPlugin.m
+++ ios/Classes/X5WebViewPlugin.m
@@ -2,6 +2,19 @@
 
 @implementation X5WebViewPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftX5WebViewPlugin registerWithRegistrar:registrar];
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"x5_webview"
+            binaryMessenger:[registrar messenger]];
+  X5WebViewPlugin* instance = [[X5WebViewPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
 }
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
 @end
